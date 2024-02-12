ac138fdd3ace0471d36fa616dfc0ed6300db9611
Asif Jahan <asifjahan307@gmail.com>
2024-02-12 21:35:15 +0600

    updated Bus Schedule time

--- google_map_demo/lib/pages/schedule_page.dart
+++ google_map_demo/lib/pages/schedule_page.dart
@@ -15,6 +15,7 @@ class _SchedulePageState extends State<SchedulePage> {
   late String _departureType;
   late DateTime _friday7AM;
   late DateTime _friday7PM;
+  bool _isBlinking = false;
 
   @override
   void initState() {
@@ -41,15 +42,28 @@ class _SchedulePageState extends State<SchedulePage> {
 
   void _updateDepartureTime() {
     DateTime now = DateTime.now();
-    if (now.isAfter(_friday7PM)) {
-      _nextDepartureTime = _getNextFriday(DateTime(7, 0));
-      _departureType = 'Bus departure: Friday 7:00 AM';
-    } else if (now.isAfter(_friday7AM)) {
-      _nextDepartureTime = _friday7PM;
-      _departureType = 'Next departure: Friday 7:00 PM';
+    if (now.weekday == DateTime.friday) {
+      if ((now.hour == 6 && now.minute >= 45) ||
+          (now.hour == 7 && now.minute < 0)) {
+        // If it's 15 minutes before Friday 7:00 AM
+        _nextDepartureTime = _friday7AM;
+        _departureType = 'Bus departure: Friday 7:00 AM';
+        _isBlinking = true;
+      } else if (now.hour >= 7 && now.hour < 19) {
+        // If it's between Friday 7:00 AM and Friday 7:00 PM
+        _nextDepartureTime = _friday7PM;
+        _departureType = 'Next departure: Friday 7:00 PM';
+        _isBlinking = false;
+      } else {
+        // If it's after Friday 7:00 PM
+        _nextDepartureTime = _getNextFriday(DateTime(7, 0));
+        _departureType = 'Bus departure: Friday 7:00 AM';
+        _isBlinking = false;
+      }
     } else {
       _nextDepartureTime = _friday7AM;
       _departureType = 'Bus departure: Friday 7:00 AM';
+      _isBlinking = false;
     }
   }
 
@@ -59,15 +73,6 @@ class _SchedulePageState extends State<SchedulePage> {
     return DateTime(today.year, today.month, today.day + daysUntilNextFriday,
         time.hour, time.minute);
   }
-
-  // String _formatDuration(Duration duration) {
-  //   if (duration.inDays >= 1) {
-  //     return '${duration.inDays} days ${duration.inHours.remainder(24)}:${duration.inMinutes.remainder(60)}:${duration.inSeconds.remainder(60)}';
-  //   } else {
-  //     String twoDigits(int n) => n.toString().padLeft(2, '0');
-  //     return '${twoDigits(duration.inHours)}:${twoDigits(duration.inMinutes)}:${twoDigits(duration.inSeconds)}';
-  //   }
-  // }
 
   String _formatDuration(Duration duration) {
     int days = duration.inDays;
@@ -103,95 +108,57 @@ class _SchedulePageState extends State<SchedulePage> {
           Navigator.pop(context);
           return false; // Prevent default back navigation
         },
-        child: Container(
-          color: Colors.white,
-          child: Scaffold(
-            appBar: AppBar(
-              //backgroundColor: Colors.blue,
-              //elevation: 0,
-              title: const Text(
-                'Bus Schedule',
-                style: TextStyle(
-                  //fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.3,
-                ),
-              ),
-              centerTitle: true,
-              leading: IconButton(
-                icon: Icon(Icons.arrow_back_ios),
-                onPressed: () {
-                  // Navigate back to the map page
-                  Navigator.pop(context);
-                },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Bus Schedule',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.3,
               ),
             ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    _departureType,
-                    style: const TextStyle(
-                      color: Colors.red,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    ),
+            centerTitle: true,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                // Navigate back to the map page
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                AnimatedDefaultTextStyle(
+                  duration: const Duration(milliseconds: 500),
+                  style: TextStyle(
+                    color: _isBlinking ? Colors.black : Colors.red,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(height: 20),
-                  // const Text(
-                  //   'until',
-                  //   style: TextStyle(
-                  //     color: Colors.deepPurple,
-                  //     fontSize: 25,
-                  //     fontWeight: FontWeight.bold,
-                  //   ),
-                  // ),
-                  Flexible(
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 15),
-                      child: Text(
-                        _formatDuration(remainingTime),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.height * 0.1,
-                          fontWeight: FontWeight.bold,
-                          color: remainingTime.inMinutes <= 15
-                              ? Colors.red
-                              : Colors.black,
-                        ),
+                  child: Text(_departureType),
+                ),
+                const SizedBox(height: 20),
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: Text(
+                      _formatDuration(remainingTime),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.height * 0.1,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
                       ),
                     ),
                   ),
-
-                  // RichText(
-                  //   text: TextSpan(
-                  //     children: _getFormattedTextSpans(formattedDuration),
-                  //   ),
-                  // ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
       ),
     );
   }
-
-  // List<TextSpan> _getFormattedTextSpans(String formattedDuration) {
-  //   List<TextSpan> textSpans = [];
-  //   List<String> components = formattedDuration.split(' ');
-
-  //   for (String component in components) {
-  //     bool isNumeric = int.tryParse(component) != null;
-  //     Color color = isNumeric ? Colors.black : Colors.red;
-  //     textSpans.add(TextSpan(
-  //       text: component,
-  //       style: TextStyle(color: color),
-  //     ));
-  //     textSpans.add(TextSpan(text: ' '));
-  //   }
-
-  //   return textSpans;
-  // }
 }
