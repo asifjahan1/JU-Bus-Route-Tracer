834591428c7204932daa49dd769fd621f1117629
Asif Jahan <asifjahan307@gmail.com>
2024-03-31 12:50:07 +0600

    fixed issues for overall map UI

--- google_map_demo/lib/pages/map_page.dart
+++ google_map_demo/lib/pages/map_page.dart
@@ -6,7 +6,6 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_map_demo/pages/schedule_page.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
 import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart' as riverpod;
 
